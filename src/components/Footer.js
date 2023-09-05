ce3f320c7a07e2c95c6bfee22db0dbc705430ba0
Paras-5 <paras.ag94@gmail.com>
2023-09-05 11:31:34 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+            <p>Copyright © {new Date().getFullYear()}</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
